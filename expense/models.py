7d1e64dc46c4113dd488efc56ffb8c8e11b053da
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-12 19:56:50 +0300

    added the templates directory with the index.html and created the index endponit which takes in an error so that it could be shown to the user

--- expense/models.py
+++ expense/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Expense(models.Model):
     expense = models.CharField(max_length=60)
     price = models.FloatField()
--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
 
-def index(request):
-    return HttpResponse("hello, world")
+from .models import Expense
+
+def index(request: HttpRequest):
+    expenses = Expense.objects.all()
+    error = request.GET.get("error")
+    return render(request, "expense/index.html", {"expenses": expenses, "error": error})
