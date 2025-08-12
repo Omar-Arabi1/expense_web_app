a6c84cc5a06c9d25b9303c0bffb3955c65c0d72e
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-13 02:09:51 +0300

    removed the error message that was sent with the request

--- expense/views.py
+++ expense/views.py
@@ -1,34 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseRedirect
 from django.urls import reverse
+from django.core.exceptions import BadRequest
 
 from .models import Expense
 
+
 def index(request: HttpRequest):
     expenses = Expense.objects.all()
-    error = request.GET.get("error")
-    return render(request, "expense/index.html", {"expenses": expenses, "error": error})
+    return render(request, "expense/index.html", {"expenses": expenses})
 
 
 def add_expense(request: HttpRequest):
     new_expense = request.POST.get("expense")
     price = request.POST.get("price")
+    if price is None or new_expense is None:
+        raise BadRequest("did not get expected fields")
+
     try:
         price = float(price)
     except Exception:
-        err_message = "price is invalid"
-        target_url = f"{reverse("expense:index")}?error={err_message}"
-        return redirect(target_url)
+        raise BadRequest("invalid price")
 
     if price <= 0:
-        err_message = "price is less than or equal to zero"
-        target_url = f"{reverse("expense:index")}?error={err_message}"
-        return redirect(target_url)
+        raise BadRequest("price is less than or equal to zero")
     if new_expense.strip() == "":
-        err_message = "expense is empty"
-        target_url = f"{reverse("expense:index")}?error={err_message}"
-        return redirect(target_url)
-
+        raise BadRequest("expense is empty")
     new_expense = Expense(expense=new_expense, price=price)
     new_expense.save()
     return HttpResponseRedirect(reverse("expense:index"))
