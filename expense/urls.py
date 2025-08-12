fd3a751931b7b78be10681cb9d839319796ba988
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-12 20:21:38 +0300

    added a way to handle the errors and added the add endpoint and view all endpoint and the basic templating in the html fle

--- expense/urls.py
+++ expense/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "expense"
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path('add_expense', views.add_expense, name="add_expense"),
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Expense
 
@@ -7,3 +8,27 @@ def index(request: HttpRequest):
     expenses = Expense.objects.all()
     error = request.GET.get("error")
     return render(request, "expense/index.html", {"expenses": expenses, "error": error})
+
+
+def add_expense(request: HttpRequest):
+    new_expense = request.POST.get("expense")
+    price = request.POST.get("price")
+    try:
+        price = float(price)
+    except Exception:
+        err_message = "price is invalid"
+        target_url = f"{reverse("expense:index")}?error={err_message}"
+        return redirect(target_url)
+
+    if price <= 0:
+        err_message = "price is less than or equal to zero"
+        target_url = f"{reverse("expense:index")}?error={err_message}"
+        return redirect(target_url)
+    if new_expense.strip() == "":
+        err_message = "expense is empty"
+        target_url = f"{reverse("expense:index")}?error={err_message}"
+        return redirect(target_url)
+
+    new_expense = Expense(expense=new_expense, price=price)
+    new_expense.save()
+    return HttpResponseRedirect(reverse("expense:index"))
